ab4017149052745cfc2be9fcfb783c9799241247
Oracle Public Cloud User <opc@spatialvm.subnet07031741.vcn07031741.oraclevcn.com>
2023-07-03 23:28:03 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width :'100%', height:'100%'}}>
+      <iframe style={{display: 'block', width: '100%', border: 'none', overflowY: 'auto',overflowX: 'hidden',height: '580px'}} allowFullScreen sandbox="allow-same-origin allow-scripts allow-popups allow-forms" src="https://129.159.142.51:4040/spatialstudio/ppp.html?proj_id=946ca7e25bbd71d3f17f7a6349f78c46&ui_elements=minimal"></iframe>
     </div>
   );
 }
